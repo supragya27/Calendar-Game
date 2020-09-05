0021bbe5464889e89c48295d1c60ff20fe54b7cc
supragya27 <supragya.chauhan001@gmail.com>
2020-09-06 00:23:53 +0530

    Updates

---
+++ precache-manifest.7cdb6e067ba03b80b4ad808c938fab21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da24d59605adcb5e4b1df9afbb5b72a",
+    "url": "/Calendar-Game/index.html"
+  },
+  {
+    "revision": "afcdd7686a4e72693dc9",
+    "url": "/Calendar-Game/static/css/main.4293e3f9.chunk.css"
+  },
+  {
+    "revision": "b879e40d4d9d72b1dd97",
+    "url": "/Calendar-Game/static/js/2.c569cd8f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Calendar-Game/static/js/2.c569cd8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afcdd7686a4e72693dc9",
+    "url": "/Calendar-Game/static/js/main.93afee5a.chunk.js"
+  },
+  {
+    "revision": "21c4af507b4de143a007",
+    "url": "/Calendar-Game/static/js/runtime-main.1c1670d3.js"
+  }
+]);
